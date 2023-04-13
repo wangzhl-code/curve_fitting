1beaa240d4f3804da00b0dc129e046eae1616ea8
wzl <1321500393@qq.com>
2023-04-13 16:19:12 +0800

    first commit

---
+++ curve_fitting.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "/usr/include/eigen3/Eigen/Dense"
+ using namespace Eigen;
+ int main()
+{
+    // 打开数据文件
+    std::ifstream file("data.txt");
+    if (!file) {
+        std::cerr << "无法打开文件！" << std::endl;
+        return 1;
+    }
+     // 读取数据点
+    int n = 0; // 数据点数
+    double x, y;
+    std::vector<double> x_vec, y_vec; // 存储x和y
+    while (file >> x >> y) {
+        ++n;
+        x_vec.push_back(x);
+        y_vec.push_back(y);
+    }
+     // 计算不同次数多项式拟合的系数
+    for (int degree = 1; degree <= 5; ++degree) {
+        // 构造矩阵A和向量b
+        MatrixXd A(n, degree + 1);
+        VectorXd b(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j <= degree; ++j) {
+                A(i, j) = std::pow(x_vec[i], j);
+            }
+            b(i) = y_vec[i];
+        }
+        // 求解最小二乘问题
+        VectorXd coeff = A.colPivHouseholderQr().solve(b);
+        // 输出结果
+        std::cout << degree << "次项式系数: ";
+        for (int i = degree; i >= 0; --i) {
+            std::cout << coeff(i) << " ";
+        }
+        std::cout << std::endl;
+    }
+     return 0;
+}
